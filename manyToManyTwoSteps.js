9936f3a0dac42ec790ea7d0d379633f47bbc5d16
Ruben Romero <baaldev.solutions@gmail.com>
2019-10-05 12:06:09 +0200

    Added manyToManyTwoSteps where we created a m:n relathionship in two steps

---
+++ manyToManyTwoSteps.js
@@ -0,0 +1,120 @@
+const sequelize = require('./sequelize')
+const Sequelize = require('sequelize');
+const DATA = require('./DataManyToMany');
+const getRandomInt = require('./helpers');
+
+
+const Users = sequelize.define('user', {
+    userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+          type: Sequelize.STRING,
+      }, 
+
+    } 
+);
+  
+const Favorites = sequelize.define('favorites', {
+    favoritesId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: Sequelize.INTEGER,
+    carId: Sequelize.INTEGER, 
+    date: {
+      type: Sequelize.DATE,
+    }
+  }
+);
+
+  const Cars = sequelize.define('cars', {
+    carId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      make: Sequelize.STRING
+  })
+  
+  Users.hasMany(Favorites, {
+    foreignKey: 'userId',
+    constraints: true,
+    delete: 'CASCADE'
+  })
+
+  Favorites.belongsTo(Users, {
+    foreignKey: 'userId',
+    constraints: true,
+  });
+
+  Cars.hasMany(Favorites, {
+    foreignKey: 'carId',
+    constraints: true,
+    delete: 'CASCADE'
+  })
+
+  Favorites.belongsTo(Cars, {
+    foreignKey: 'carId',
+    constraints: true,
+  });
+
+
+const intiDataBase = async () => {
+    await sequelize.sync({force: true});
+}
+
+const run = async () => {
+    const max = 3;
+    await intiDataBase();
+
+    await Promise.all( DATA.users.map( async elem=> 
+         Users.create({...elem})
+    ))
+
+    await Promise.all( DATA.cars.map( async elem => 
+        Cars.create({...elem})
+   ))
+
+   for( let i = 0 ; i < 5 ; i++ ){
+       Favorites.create({
+           userId: getRandomInt(1 , max),
+           carId: getRandomInt(1, max)
+       })
+   }
+
+
+   const cars = await Users.findAll({
+       include: {
+        model: Favorites,
+        attributes: [ 'userId', 'carId'],
+    },
+    raw: true
+   });
+   console.log('Favorites cars for all users')
+   console.log(cars);
+
+   
+
+   const users = await Cars.findAll({
+        include: {
+        model: Favorites,
+        attributes: [ 'userId', 'carId'],
+        },
+        raw: true
+    });
+
+    
+    console.log('Users wiht its favorites cars')
+    console.log(users);
+
+
+} 
+  
+run();
+
